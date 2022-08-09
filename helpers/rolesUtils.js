2630db130e376780971801749776ee67bdc4507a
YipChiSan <zye39@yahoo.com>
2022-08-09 21:04:24 +1000

    Finish helper functions for roles

--- helpers/rolesUtils.js
+++ helpers/rolesUtils.js
@@ -1,11 +1,32 @@
-function getAllRoles() {
+const db = require('./createdb');
 
+function getAllRoles() {
+    db.query(`SELECT * FROM role_table`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(results);
+    });
 }
 
 function addRoles(title, salary, department_id) {
-
+    db.query(`INSERT INTO role_table (title, salary, department_id) VALUES  (?, ?, ?),`, [title, salary, department_id], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(result);
+        }
+    );
 }
 
 function deleteRoles(role_id) {
-
+    db.query(`DELETE FROM role_table where id = ?,`, [role_id], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(result);
+    }
+    )
 }
+
+module.exports = {getAllRoles, addRoles, deleteRoles};
