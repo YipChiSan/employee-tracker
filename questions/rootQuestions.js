5396a886465d92a05eab98802f6134b58c9a1eec
YipChiSan <zye39@yahoo.com>
2022-08-09 21:19:29 +1000

    Finish root questions list

---
+++ questions/rootQuestions.js
@@ -0,0 +1,22 @@
+const rootQuestion = [
+    {
+        type: 'list',
+        message: "What would you like to do?",
+        choices: ["View all employees", 
+                    "Add employees", 
+                    "Update employee's role", 
+                    "Update employee's manager", 
+                    "View employee's by manager",
+                    "View employees by department",
+                    "Delete employee",
+                    "View all departments",
+                    "Add department",
+                    "Delete department",
+                    "View summed salaries of all employees in a department",
+                    "View all roles",
+                    "Add role",
+                    "Delete role",
+                    "Quit"],
+        name: "basicAction",
+    }
+];
