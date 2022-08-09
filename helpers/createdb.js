0270eea08910a52666c032ca2faac91284418e96
YipChiSan <zye39@yahoo.com>
2022-08-09 19:41:45 +1000

    Create a connection to database

--- helpers/createdb.js
+++ helpers/createdb.js
@@ -1,10 +1,11 @@
-function createdb () {
-    const mysql = require('mysql2');
 
-    const con = mysql.createConnection({
-        host: 'localhost',
-        user: 'test',
-        database: 'employees_db',
-        password: 'newpassword'
-    });
-}
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'test',
+    database: 'employees_db',
+    password: 'newpassword'
+});
+
+module.exports = db;
