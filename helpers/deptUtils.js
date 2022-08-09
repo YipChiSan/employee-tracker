9bb1c6de0529355e3e5ab42dd4b18032b6eaa4fd
YipChiSan <zye39@yahoo.com>
2022-08-09 19:58:32 +1000

    Finish helper functions for department

--- helpers/deptUtils.js
+++ helpers/deptUtils.js
@@ -1,14 +1,32 @@
-
+const db = require('./createdb');
 
 
 function getAllDepartments() {
-
+    db.query(`SELECT * FROM department`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(results);
+    });
 }
 
 function addDepartment(name) {
-
-}
+    db.query(`INSERT INTO department (name) VALUES  (?),`, [name], (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+    }
+)}
 
 function deleteDept(dept_id) {
-
+    db.query(`DELETE FROM department where id = ?,`, [dept_id], (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+    }
+)
 }
+
+module.exports = {getAllDepartments, addDepartment, deleteDept};
