3e718551f3191385a80221abf854fd04af7f3077
YipChiSan <zye39@yahoo.com>
2022-08-10 16:07:46 +1000

    Finish addDeptQuestions

---
+++ questions/addDeptQuestions.js
@@ -0,0 +1,10 @@
+
+const addDeptQuestions = [
+    {
+        type: 'input',
+        message: 'What is the name of the new department?',
+        name: 'name',
+    }
+];
+
+module.exports = addDeptQuestions;
