38194ca8873e5ecb89e47cce18898716420a3796
YipChiSan <zye39@yahoo.com>
2022-08-11 23:16:45 +1000

    Change query to promise

--- helpers/deptUtils.js
+++ helpers/deptUtils.js
@@ -4,50 +4,31 @@ const mysql = require('mysql2');
 
 function getAllDepartments() {
     return db.query(`SELECT * FROM department`);
-    
-   
 }
 
 function addDepartment(name) {
-    db.query(`INSERT INTO department (name) VALUES  (?),`, [name], (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        return result;
-    }
-    }
-)}
+    return db.query(`INSERT INTO department (name) VALUES  (?),`, [name]);
+}
 
 function deleteDept(dept_id) {
     dept_id = getIDFromCombinedData(dept_id);
-    db.query(`DELETE FROM department where id = ?,`, [dept_id], (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        return result;
-    }
-    }
-)
+    return db.query(`DELETE FROM department where id = ?,`, [dept_id]);
+
 }
 
 function getSalariesBydept(dept_id) {
     dept_id = getIDFromCombinedData(dept_id);
-    db.query(`SELECT SUM(role_table.salary) as total_salary, department.name as department_name
+    return db.query(`SELECT total_salary, department_name
+                FROM(SELECT SUM(role_table.salary) as total_salary, department.id, department.name as department_name
                 FROM employee 
                 INNER JOIN role_table 
                 ON employee.role_id = role_table.id 
                 INNER JOIN department
                 ON role_table.department_id = department.id
                 GROUP BY department_name
-                HAVING department.id = ?`, [dept_id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return result;
-        }
-        }
-    );
+                HAVING department.id = ?) AS temp
+`, [dept_id]);
 }
 
-getAllDepartments().then( ([rows]) => { console.log(rows)} ).then(() => db.end());
+getSalariesBydept("1.aaa").then( ([rows]) => { console.log(rows)} ).then(() => db.end());
 module.exports = {getAllDepartments, addDepartment, deleteDept, getSalariesBydept};
