26f25331f05bea5ae3a5777b8f1b5cd8ff564ccf
YipChiSan <zye39@yahoo.com>
2022-08-11 21:09:41 +1000

    Fix a typo

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE role_table (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department)
+    FOREIGN KEY (department_id)
     REFERENCES department(id)
     on DELETE SET NULL
 ); 
