ca6761445677b15279de966a0929bd0fe7d17cef
YipChiSan <zye39@yahoo.com>
2022-08-12 13:19:48 +1000

    Change query to promise

--- helpers/employeeUtils.js
+++ helpers/employeeUtils.js
@@ -2,98 +2,50 @@ const getIDFromCombinedData = require('./getIDFromCombinedData');
 const db = require('./createdb');
 
 function getAllEmployees() {
-    db.query(`SELECT id, first_name, last_name FROM employee`, (err, results) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return results;
-        }
-        
-    });
+    return db.query(`SELECT id, first_name, last_name FROM employee`);
 }
 
 function addEmployee(first_name, last_name, role_id, manager_id) {
     role_id = getIDFromCombinedData(role_id);
     manager_id = getIDFromCombinedData(manager_id);
-    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES  (?, ?, ?, ?),`, [first_name, last_name, role_id, manager_id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return result;
-        }
-        }
-    );
+    return db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES  (?, ?, ?, ?),`);
 }
 
 function updateEmployeeRole(employee_id, role_id) {
     employee_id = getIDFromCombinedData(employee_id);
     role_id = getIDFromCombinedData(role_id);
-    db.query(`UPDATE employee SET role_id = ? WHERE employee_id = ?;`, [role_id, employee_id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return result;
-        }
-        }
-    );
+    return db.query(`UPDATE employee SET role_id = ? WHERE employee_id = ?;`, [role_id, employee_id]);
 }
 
 function updateEmployeeManager(employee_id, manager_id) {
     employee_id = getIDFromCombinedData(employee_id);
     manager_id = getIDFromCombinedData(manager_id);
-    db.query(`UPDATE employee SET manager_id = ? WHERE employee_id = ?;`, [manager_id, employee_id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return result;
-        }
-        }
-    );
+    return db.query(`UPDATE employee SET manager_id = ? WHERE employee_id = ?;`, [manager_id, employee_id]);
 }
 
 function getEmployeesByManager(manager_id) {
     manager_id = getIDFromCombinedData(manager_id);
-    db.query(`SELECT employee.id as id, employee.first_name as first_name, employee.last_name as last_name, role_table.title as title 
+    return db.query(`SELECT employee.id as id, employee.first_name as first_name, employee.last_name as last_name, role_table.title as title 
                 FROM employee 
                 LEFT JOIN role_table 
                 ON employee.role_id = role_table.id 
-                WHERE employee.manager_id = ?`, [manager_id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return result;
-        }
-        }
-    );
+                WHERE employee.manager_id = ?`, [manager_id]);
 }
 
 function getEmployeesByDept(dept_id) {
     dept_id = getIDFromCombinedData(dept_id);
-    db.query(`SELECT employee.id as id, employee.first_name as first_name, employee.last_name as last_name, role_table.title as title 
+    return db.query(`SELECT employee.id as id, employee.first_name as first_name, employee.last_name as last_name, role_table.title as title 
                 FROM employee 
                 INNER JOIN role_table 
                 ON employee.role_id = role_table.id 
                 INNER JOIN department
                 ON role_table.department_id = department.id
-                WHERE department.id = ?`, [dept_id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return result;
-        }
-        }
-    );
+                WHERE department.id = ?`, [dept_id]);
 }
 
 function deleteEmployee(employee_id) {
     employee_id = getIDFromCombinedData(employee_id);
-    db.query(`DELETE FROM employee WHERE id = ?`, [employee_id],  (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return result;
-        }
-    });
+    return db.query(`DELETE FROM employee WHERE id = ?`, [employee_id]);
 }
 
 module.exports = {getAllEmployees, addEmployee, updateEmployeeRole, updateEmployeeManager, getEmployeesByManager, getEmployeesByDept, deleteEmployee};
--- helpers/rolesUtils.js
+++ helpers/rolesUtils.js
@@ -2,37 +2,17 @@ const db = require('./createdb');
 const getIDFromCombinedData = require('./getIDFromCombinedData');
 
 function getAllRoles() {
-    db.query(`SELECT * FROM role_table`, (err, results) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return results;
-        }
-    });
+    return db.query(`SELECT * FROM role_table`);
 }
 
 function addRoles(title, salary, department_id) {
     department_id = getIDFromCombinedData(department_id);
-    db.query(`INSERT INTO role_table (title, salary, department_id) VALUES  (?, ?, ?),`, [title, salary, department_id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return result;
-        }
-        }
-    );
+    return db.query(`INSERT INTO role_table (title, salary, department_id) VALUES  (?, ?, ?),`, [title, salary, department_id]);
 }
 
 function deleteRoles(role_id) {
     role_id = getIDFromCombinedData(role_id);
-    db.query(`DELETE FROM role_table where id = ?,`, [role_id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return result;
-        }
-    }
-    )
+    return db.query(`DELETE FROM role_table where id = ?,`, [role_id]);
 }
 
 module.exports = {getAllRoles, addRoles, deleteRoles};
